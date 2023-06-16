24a0d86af8685a6a34c63d30921e0eb3d48f14c7
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2023-06-16 08:24:04 +0300

    Initial

---
+++ anti_longread_bot.py
@@ -0,0 +1,23 @@
+import telebot
+from environs import Env
+
+
+env: Env = Env()
+env.read_env()
+
+bot = telebot.TeleBot(env('ALR_TOKEN'))
+
+@bot.message_handler(is_chat_admin=True, commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id,
+                         "Hi, i will protect you from longreads")
+
+@bot.message_handler(func=lambda message: message.forward_from_chat and len(message.text) > 500)
+def handle_long_message(message):
+    # Delete the long forwarded message
+    bot.delete_message(message.chat.id, message.message_id)
+    # Send the link to the original message
+    original_link = f"https://t.me/{message.forward_from_chat.username}/{message.forward_from_message_id}"
+    bot.send_message(message.chat.id, original_link)
+
+bot.polling()
