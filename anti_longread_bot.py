ea0926fb850a3439867bfcd7893489f65fb31130
Cheaster121 <cheater1211@gmail.com>
2023-06-25 20:08:14 +0300

    codestyle

--- anti_longread_bot.py
+++ anti_longread_bot.py
@@ -1,16 +1,17 @@
 import telebot
 from environs import Env
 
-
 env: Env = Env()
 env.read_env()
 
 bot = telebot.TeleBot(env('ALR_TOKEN'))
 
+
 @bot.message_handler(is_chat_admin=True, commands=['start'])
 def start_handler(message):
     bot.send_message(message.chat.id,
-                         "Hi, i will protect you from longreads")
+                     "Hi, i will protect you from long-reads")
+
 
 @bot.message_handler(func=lambda message: message.forward_from_chat and len(message.text) > 500)
 def handle_long_message(message):
@@ -19,5 +20,6 @@ def handle_long_message(message):
     # Send the link to the original message
     original_link = f"https://t.me/{message.forward_from_chat.username}/{message.forward_from_message_id}"
     bot.send_message(message.chat.id, original_link)
+
 
 bot.polling()
